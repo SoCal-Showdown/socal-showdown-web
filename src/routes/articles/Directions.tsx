f5e9fc886494044c317fccdecc2037a4f98e5b56
exampleCoderOne <exampleemail3001@gmail.com>
2023-09-22 17:57:39 -0700

    Replacing maps
    unfinished!!

--- src/routes/articles/Directions.tsx
+++ src/routes/articles/Directions.tsx
@@ -1,6 +1,9 @@
 import eventMapBig from './images/EventMapBig.jpg';
-import eventMapSmall from './images/EventMapSmall.jpg';
+import eventMapSmallex from './images/EventMapSmall.jpg';
 import eventMapBigTwo from './images/EventMapBig2.jpg';
+import eventMapBigThree from './images/Copy of Event Map Big.jpg';
+import eventmaplarge from './images/EventMapLarge.jpg';
+import eventmapsmall from './images/Event Map Small (6).jpg';
 
 function Directions() {
     return (
@@ -9,17 +12,19 @@ function Directions() {
                 Directions
             </h1>
 
-            <div className='m-5 flex flex-wrap justify-center gap-10'>
-                <img
-                    src={eventMapBigTwo}
-                    alt='Event map big'
-                    className='w-1/2'></img>{' '}
-                {/*957 x 704*/}
-                <img
-                    src={eventMapSmall}
-                    alt='Event map big'
-                    className='w-1/3'></img>{' '}
-                {/*485x717*/}
+            <div className='flex w-fit flex-wrap justify-center gap-2'>
+                <div className='h-80 w-3/5'>
+                    <img
+                        src={eventmaplarge}
+                        alt='Event map big'
+                        className='p-3 '></img>
+                </div>
+                <div className='w-1/4'>
+                    <img
+                        src={eventmapsmall}
+                        alt='Event map big'
+                        className='p-3'></img>
+                </div>
             </div>
         </main>
     );
