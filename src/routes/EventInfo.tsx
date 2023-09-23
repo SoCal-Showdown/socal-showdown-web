742bd295608a98d08bae1ae96a37185ba63710d0
Phong <phongd.nguyen2009@gmail.com>
2023-09-23 10:04:55 -0700

    Changed and centered Iframe height

--- src/routes/EventInfo.tsx
+++ src/routes/EventInfo.tsx
@@ -4,7 +4,7 @@ function EventInfo() {
             <h1 className='p-3 text-center font-serif text-2xl font-bold text-bluegray'>
                 Event Info
             </h1>
-            <div className='w-96 bg-orange-600'>
+            <div className='h-auto w-auto bg-orange-600'>
                 <iframe
                     src='https://docs.google.com/document/d/1xpXQ2m-6wnE8lJ7vfZGyj5LZa2_FiiNrmf5Rd18gKRM/edit?pli=1ci%20Schools!5e0!3m2!1sen!2sus!4v1693364360555!5m2!1sen!2sus'
                     loading='lazy'
