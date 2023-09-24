8b20109de18376eeaf83616003a1baf0dcbbd3e2
Phong Nguyen <phongd.nguyen2009@gmail.com>
2023-09-24 08:18:16 -0700

    embeded new Iframe for the schedule

--- src/routes/EventInfo.tsx
+++ src/routes/EventInfo.tsx
@@ -7,14 +7,14 @@ function EventInfo() {
             <h2 className='text-1.5xl text-center font-serif font-semibold'>
                 Schedule
             </h2>
-            <div className='flex h-screen w-screen flex-wrap justify-center gap-2 bg-orange-600'>
+            <div className='flex h-screen w-screen flex-wrap justify-center gap-2'>
                 <iframe
-                    src='https://docs.google.com/document/d/1xpXQ2m-6wnE8lJ7vfZGyj5LZa2_FiiNrmf5Rd18gKRM/edit?pli=1ci%20Schools!5e0!3m2!1sen!2sus!4v1693364360555!5m2!1sen!2sus'
-                    loading='lazy'
-                    className='m-2 h-2/3 w-2/3'
-                    title='Schedule'></iframe>
+                    src='https://docs.google.com/document/d/e/2PACX-1vSonp2hI3h6Ccq2kjgTZASQOx7y7RZmveAx-H2ebVo7GeEZjb16MO5l3oTwMeA_KiHhGXQuPDfGfswN/pub?embedded=true'
+                    className='h-fill m-2 w-1/2 align-baseline'
+                    title='Schedule'>
+                    /
+                </iframe>
             </div>
-            <div className='m-5 h-max w-fit max-w-md p-7 pt-0 leading-9'></div>
         </main>
     );
 }
