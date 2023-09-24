dfe03aad7f189abf67c08f40cee2978b9b4b9913
Phong <phongd.nguyen2009@gmail.com>
2023-09-24 15:15:06 -0700

    added workshop schedule to event info page

--- src/routes/EventInfo.tsx
+++ src/routes/EventInfo.tsx
@@ -4,9 +4,7 @@ function EventInfo() {
             <h1 className='p-3 text-center font-serif text-2xl font-bold text-bluegray'>
                 Event Info
             </h1>
-            <h2 className='text-1.5xl text-center font-serif font-semibold'>
-                Schedule
-            </h2>
+
             <div className='flex h-screen w-screen flex-wrap justify-center gap-2'>
                 <iframe
                     src='https://docs.google.com/document/d/e/2PACX-1vSonp2hI3h6Ccq2kjgTZASQOx7y7RZmveAx-H2ebVo7GeEZjb16MO5l3oTwMeA_KiHhGXQuPDfGfswN/pub?embedded=true'
@@ -14,6 +12,14 @@ function EventInfo() {
                     title='Schedule'>
                     /
                 </iframe>
+            </div>
+            <div>
+                <div className='flex h-screen w-screen flex-wrap justify-center gap-2'>
+                    <iframe
+                        src='https://docs.google.com/document/d/e/2PACX-1vQ2LEGaNxRovzOHp3jL0m2eJTdmoZHDVgojgcu2vKEQObzco_hymSiDAz38bdKwtJab15zNdYdUgQuE/pub?embedded=true'
+                        className='h-fill m-2 w-2/3 align-baseline'
+                        title='2023 Workshop Schedule'></iframe>
+                </div>
             </div>
         </main>
     );
