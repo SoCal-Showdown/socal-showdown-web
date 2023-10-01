2410a42372e427f9b749e32690ddc59d69a12c3f
exampleCoderOne <exampleemail3001@gmail.com>
2023-10-01 16:36:32 -0700

    Added HeaderImage reference

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -2,13 +2,14 @@
 import { Outlet } from 'react-router';
 import { Link } from 'react-router-dom';
 import SocalHassLogo from './articles/images/SocalHassLogo.png';
+import HeaderImage from './articles/HeaderImage';
 
 function Root() {
     return (
         <>
-            <header>                
+            <header>
                 <HeaderImage pageTitle='Socal Showdown' />
-                    
+
                 <div className='m-5 h-fit items-center gap-40 border-purple bg-white'>
                     <nav className='ml-5 flex flex-row items-center justify-end gap-4 pr-5 text-center font-serif text-2xl'>
                         <img
@@ -23,9 +24,7 @@ function Root() {
                         <Link to='sponsors'>Sponsors</Link>
                     </nav>
                 </div>
-
             </header>
-            
         </>
     );
 }
