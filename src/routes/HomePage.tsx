e5a5267cd57bf291c1e54717aa841f23bc898801
Crisanto Carrillo <crisanto.e.carrillo@gmail.com>
2023-10-02 14:16:41 -0700

    Header of So-Cal showdown website (starting tabs)

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,7 +1,20 @@
 function HomePage() {
     return (
         <main>
-            <h1>Homepage</h1>
+            <div className="grid place-content-center bg-[#6d22b6] py-4">
+                <button className="px-6 py-1 border-solid border font-['Open_Sans'] text-[18px] border-white text-white rounded">See Latest Announcements</button>
+            </div>
+            <nav>
+                <button className="px-8 py-2"><img src="SoCalShowdown.png" className="object-scale-down h-10 w-30" /></button>
+                <button className="bg-black text-white px-[8px] -translate-y-[15px] font-['Open_Sans'] text-[17px]">Home</button>
+                {/* Team Dropdown Menu vvv */}
+                <button id="teamDropdown" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" className="px-[10px] -translate-y-[15px] font-['Open_Sans'] text-[17px]">Teams</button>              
+                
+            </nav>
+            <div className="bg-zinc-800">
+                <h1 className="absolute text-white left-1/2 -translate-x-1/2 translate-y-[75px] font-[Oswald] font-[900] text-[60pt] z-10 underline underline-offset-[20px] decoration-[#6d22b6] decoration-[13px]">SOCAL SHOWDOWN</h1>
+                <img src="CompetitionPhoto.png" className="h-[285px] w-full object-cover object-center opacity-50 z-0" />
+            </div>
         </main>
     );
 }
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -4,18 +4,6 @@ import { Link } from 'react-router-dom';
 function Root() {
     return (
         <>
-            <header>
-                <p className='p-3 text-center font-serif text-3xl font-bold text-bluegray'>
-                    Socal Showdown
-                </p>
-                <nav className='mt-2 flex flex-row justify-center gap-4 text-center font-serif text-2xl'>
-                    <Link to=''>Home</Link>
-                    <Link to='signup'>Sign Up</Link>
-                    <Link to='location_time'>Location Time</Link>
-                    <Link to='directions'>Directions</Link>
-                    <Link to='sponsors'>Sponsors</Link>
-                </nav>
-            </header>
             <Outlet />
         </>
     );
