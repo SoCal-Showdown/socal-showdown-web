d1bf1de8f1e5649e162f17d57f84533c3c2893d6
koro <drganeelmegumin23@gmail.com>
2023-10-03 16:45:37 -0700

    game rules added :3'

--- src/routes/articles/GameRules.tsx
+++ src/routes/articles/GameRules.tsx
@@ -4,6 +4,15 @@ function GameRules() {
     return (
         <main>
             <HeaderImage pageTitle='Game Rules' />
+
+            <div>
+                <div className='flex h-screen w-screen flex-wrap justify-center gap-2'>
+                    <iframe
+                        src='https://docs.google.com/document/d/1qfFO4ZkWHIzBz9NjZfKxwAYl8sCX2xr9GMahSqRAv1A/edit'
+                        className='h-fill m-2 w-2/3 align-baseline'
+                        title='Volunteer Sing up '></iframe>
+                </div>
+            </div>
         </main>
     );
 }
