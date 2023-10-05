505b1ddbad12f7523b92ac6a846bb7310887b56c
Crisanto Carrillo <crisanto.e.carrillo@gmail.com>
2023-10-05 10:14:41 -0700

    the drop down is upgraded!

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,4 +1,4 @@
-import BasicExample from "./articles/Dropdown";
+import Drop from "./articles/Dropdown";
 
 function HomePage() {
     return (
@@ -6,10 +6,10 @@ function HomePage() {
             <div className="grid place-content-center bg-[#6d22b6] py-4">
                 <button className="px-6 py-1 border-solid border font-['Open_Sans'] text-[18px] border-white text-white rounded">See Latest Announcements</button>
             </div>
-            <div>
+            <div className="flex flex-row">
                 <button className="px-8 py-2"><img src="SoCalShowdown.png" className="object-scale-down h-10 w-30" /></button>
-                <button className="bg-black text-white px-[8px] -translate-y-[15px] font-['Open_Sans'] text-[17px]">Home</button>
-                <BasicExample></BasicExample>
+                <button className="bg-black text-white px-[8px] font-['Open_Sans'] text-[17px]">Home</button>
+                    <Drop></Drop>
             </div>
             <div className="bg-zinc-800">
                 <h1 className="absolute text-white left-1/2 -translate-x-1/2 translate-y-[75px] font-[Oswald] font-[900] text-[60pt] z-10 underline underline-offset-[20px] decoration-[#6d22b6] decoration-[13px]">SOCAL SHOWDOWN</h1>
--- src/routes/articles/Dropdown.tsx
+++ src/routes/articles/Dropdown.tsx
@@ -1,19 +1,61 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 
-function BasicExample() {
+function Teams() {
   return (
     <Dropdown>
       <Dropdown.Toggle variant="Primary" id="dropdown-basic" className='text-black bg-none border-none'>
-        Dropdown Button
+        Teams
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Item href="#/action-1">Hosts</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">TBA (Team List)</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
 }
 
-export default BasicExample;
+function GameRules() {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="Primary" id="dropdown-basic" className='text-black bg-none border-none'>
+        Game Rules
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Game Rules</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Awards</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+function EventInfo() {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="Primary" id="dropdown-basic" className='text-black bg-none border-none'>
+        Event Info
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">COVID Policy</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Discovery Row</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Maps</Dropdown.Item>
+        <Dropdown.Item href="#/action-4">Schedule</Dropdown.Item>
+        <Dropdown.Item href="#/action-5">Workshops</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+} 
+
+function Drop() {
+  return (
+    <div className='flex flex-row'>
+      <Teams></Teams>
+      <GameRules></GameRules>
+      <EventInfo></EventInfo>
+    </div>
+  );
+}
+
+export default Drop;
