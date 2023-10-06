b468d4a7af48d5a8706b49cc4c758183e7cfdc59
koro <drganeelmegumin23@gmail.com>
2023-10-06 00:07:30 -0700

    homepage format in progress

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -8,10 +8,6 @@ import robotImage from './articles/images/Robot.png';
 function HomePage() {
     return (
         <main>
-            <h1 className='text-center text-5xl font-semibold text-black'>
-                Continue your season at <br></br>
-                <b className='text-purple-700'>SoCal ShowDown!</b>
-            </h1>
             <br></br>
             <div className='flex gap-6 '>
                 <img
@@ -20,7 +16,11 @@ function HomePage() {
                     className='ml-5 w-0 flex-grow basis-0 scale-90'
                 />
                 <div className='flex-grow basis-0'>
-                    <p className='w-1/2 text-center text-lg'>
+                    <h1 className='my-8 mr-[250px] text-left font-[Oswald] text-5xl uppercase text-black'>
+                        Continue your season at <br></br>
+                        <b className='text-purple '>SoCal ShowDown!</b>
+                    </h1>
+                    <p className='w-auto text-left text-lg'>
                         Join us for a weekend of robotics action and fierce
                         competition at <b>SoCal Showdown</b>, a new off-season
                         competition held at the same venue as the Los Angeles
@@ -31,7 +31,7 @@ function HomePage() {
                         invent at <b className='underline'>Discovery Row</b>!
                         Come build a launcher, experiment with LEGO Robots, and
                         find out what FIRST Robotics Competition is all about.
-                        Don’t forget to visit booths from our sponsors to
+                        Don't forget to visit booths from our sponsors to
                         discover more about real-world engineering!
                         <br></br>
                         <br></br>
@@ -39,63 +39,55 @@ function HomePage() {
                     </p>
                 </div>
             </div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <h1 className='decoration-purple-700 text-center text-2xl text-white underline'>
-                weewooo and stuff ahhhhhhhhhhhhhhhhhhhhhh
+
+            <hr className='mx-10 my-8 h-[2.5px] w-[1200px] rounded bg-purple object-left' />
+
+            <h1 className='mx-10 my-10 text-left font-[Oswald] text-5xl uppercase'>
+                Sponsors
             </h1>
-            <br></br>
-            <br></br>
-            <h1 className='text-center text-5xl font-semibold'>Sponsors</h1>
-            <br></br>
-            <br></br>
-            <Sponsors />
-            <br></br>
-            <br></br>
-            <br></br>
-            <h1 className='decoration-purple-700 text-center text-2xl text-white underline'>
-                weewooo and stuff ahhhhhhhhhhhhhhhhhhhhhh
+
+            <div className='my-20'>
+                <Sponsors />
+            </div>
+
+            <div className='flex items-center justify-center '>
+                <hr className=' mx-10 h-[2.5px] w-[1400px]  rounded bg-purple' />
+            </div>
+
+            <h1 className='mx-[86px] my-8 text-left font-[Oswald] text-5xl'>
+                Host Teams
             </h1>
-            <br></br>
-            <br></br>
-            <h1 className='text-center text-5xl font-semibold'>Host Teams</h1>
-            <br></br>
-            <br></br>
-            <div className='bg-purple-700  flex flex-wrap items-center justify-center'>
-                <br></br>
-                <br></br>
-                <div className='flex flex-row items-center'>
+
+            <div className='my-5  flex  items-center justify-center gap-0 bg-purple'>
+                <div className='flex flex-col '>
                     <img
                         alt='Nerd Herd Group Photo'
                         src={nerdHerdImage}
-                        className='px-2m-5 mx-auto w-1/5 scale-100'
+                        className='flex flex-grow '
                     />
                     <a
                         href='https://687camsrobotics.wixsite.com/mysite'
-                        className='text-right text-2xl font-bold text-white underline'>
+                        className='text-center text-2xl font-bold text-white underline '>
                         687 - The Nerd Herd
                     </a>
-                    <br></br>
-                    <br></br>
-                    <br></br>
+                </div>
+                <div className='flex flex-col '>
                     <img
                         alt='Phantom Catz Group Photo'
                         src={phantomCatzImage}
-                        className='px-2m-5 mx-auto w-1/5 scale-100'
+                        className='flex flex-grow scale-75'
                     />
                     <a
                         href='https://www.frcteam2637.org/'
-                        className='text-2xl font-bold text-white underline'>
+                        className=' text-center text-2xl font-bold text-white underline'>
                         2637 - Phantom Catz
                     </a>
-                    <br></br>
-                    <br></br>
-                    <br></br>
+                </div>
+                <div className='flex flex-col items-center '>
                     <img
                         alt='The Vitruvian Bots Group Photo'
                         src={vitruvianBotsImage}
-                        className='px-2m-5 mx-auto w-1/5 scale-90'
+                        className='flex scale-50'
                     />
                     <a
                         href='https://team4201.org/'
