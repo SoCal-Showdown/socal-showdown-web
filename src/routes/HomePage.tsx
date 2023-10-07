aa52729591306b9dd9e3070c477e321317ae49a8
koro <drganeelmegumin23@gmail.com>
2023-10-06 23:20:21 -0700

    formatting for dropdown

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,19 +1,32 @@
-import Drop from "./articles/Dropdown";
+/* eslint-disable jsx-a11y/alt-text */
+import Drop from './articles/Dropdown';
 
 function HomePage() {
     return (
         <main>
-            <div className="grid place-content-center bg-[#6d22b6] py-4">
-                <button className="px-6 py-1 border-solid border font-['Open_Sans'] text-[18px] border-white text-white rounded">See Latest Announcements</button>
+            <div className='grid place-content-center bg-[#6d22b6] py-4'>
+                <button className="rounded border border-solid border-white px-6 py-1 font-['Open_Sans'] text-[18px] text-white">
+                    See Latest Announcements
+                </button>
             </div>
-            <div className="flex flex-row">
-                <button className="px-8 py-2"><img src="SoCalShowdown.png" className="object-scale-down h-10 w-30" /></button>
-                <button className="bg-black text-white px-[8px] font-['Open_Sans'] text-[17px]">Home</button>
-                    <Drop></Drop>
+            <div className='flex flex-row'>
+                <button className='px-8 py-2'>
+                    <img
+                        src='SoCalShowdown.png'
+                        className='w-30 h-10 object-scale-down'
+                    />
+                </button>
+
+                <Drop></Drop>
             </div>
-            <div className="bg-zinc-800">
-                <h1 className="absolute text-white left-1/2 -translate-x-1/2 translate-y-[75px] font-[Oswald] font-[900] text-[60pt] z-10 underline underline-offset-[20px] decoration-[#6d22b6] decoration-[13px]">SOCAL SHOWDOWN</h1>
-                <img src="CompetitionPhoto.png" className="h-[285px] w-full object-cover object-center opacity-50 z-0" />
+            <div className='bg-zinc-800'>
+                <h1 className='absolute left-1/2 z-10 -translate-x-1/2 translate-y-[75px] font-[Oswald] text-[60pt] font-[900] text-white underline decoration-[#6d22b6] decoration-[13px] underline-offset-[20px]'>
+                    SOCAL SHOWDOWN
+                </h1>
+                <img
+                    src='CompetitionPhoto.png'
+                    className='z-0 h-[285px] w-full object-cover object-center opacity-50'
+                />
             </div>
         </main>
     );
--- src/routes/articles/Dropdown.tsx
+++ src/routes/articles/Dropdown.tsx
@@ -1,61 +1,71 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 
 function Teams() {
-  return (
-    <Dropdown>
-      <Dropdown.Toggle variant="Primary" id="dropdown-basic" className='text-black bg-none border-none'>
-        Teams
-      </Dropdown.Toggle>
+    return (
+        <Dropdown>
+            <Dropdown.Toggle
+                variant='Primary'
+                id='dropdown-basic'
+                className='border-none bg-none text-black'>
+                Teams
+            </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Hosts</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">TBA (Team List)</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
+            <Dropdown.Menu>
+                <Dropdown.Item href='#/action-1'>Hosts</Dropdown.Item>
+                <Dropdown.Item href='#/action-2'>TBA (Team List)</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
 }
 
 function GameRules() {
-  return (
-    <Dropdown>
-      <Dropdown.Toggle variant="Primary" id="dropdown-basic" className='text-black bg-none border-none'>
-        Game Rules
-      </Dropdown.Toggle>
+    return (
+        <Dropdown>
+            <Dropdown.Toggle
+                variant='Primary'
+                id='dropdown-basic'
+                className='border-none bg-none text-black'>
+                Game Rules
+            </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Game Rules</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Awards</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
+            <Dropdown.Menu>
+                <Dropdown.Item href='#/action-1'>Game Rules</Dropdown.Item>
+                <Dropdown.Item href='#/action-2'>Awards</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
 }
 
 function EventInfo() {
-  return (
-    <Dropdown>
-      <Dropdown.Toggle variant="Primary" id="dropdown-basic" className='text-black bg-none border-none'>
-        Event Info
-      </Dropdown.Toggle>
+    return (
+        <Dropdown>
+            <Dropdown.Toggle
+                variant='Primary'
+                id='dropdown-basic'
+                className='border-none bg-none text-black'>
+                Event Info
+            </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">COVID Policy</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Discovery Row</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Maps</Dropdown.Item>
-        <Dropdown.Item href="#/action-4">Schedule</Dropdown.Item>
-        <Dropdown.Item href="#/action-5">Workshops</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
-} 
+            <Dropdown.Menu>
+                <Dropdown.Item href='#/action-1'>COVID Policy</Dropdown.Item>
+                <Dropdown.Item href='#/action-2'>Discovery Row</Dropdown.Item>
+                <Dropdown.Item href='#/action-3'>Maps</Dropdown.Item>
+                <Dropdown.Item href='#/action-4'>Schedule</Dropdown.Item>
+                <Dropdown.Item href='#/action-5'>Workshops</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+}
 
 function Drop() {
-  return (
-    <div className='flex flex-row'>
-      <Teams></Teams>
-      <GameRules></GameRules>
-      <EventInfo></EventInfo>
-    </div>
-  );
+    return (
+        <div className='mx-[1090px] flex flex-row px-[8px] text-[17px] '>
+            <button className='bg-black text-white '>Home</button>
+            <Teams></Teams>
+            <GameRules></GameRules>
+            <EventInfo></EventInfo>
+        </div>
+    );
 }
 
 export default Drop;
